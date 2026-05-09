b6ca6a9fe583dca9202539c3037b6496a4fe7207
apurvaKhade25 <apurvakhade259@gmail.com>
2026-05-10 01:28:22 +0530

    Happy number

---
+++ Two pointers/HappyNumber.py
@@ -0,0 +1,19 @@
+def happyNum(num):
+    seen=set()
+    while (num!=1):
+        if num in seen:
+            return False
+        seen.add(num)
+        total=0
+
+        while num>0:
+            last_digit=num%10
+            total+=last_digit*last_digit
+            num=num//10
+        
+        num=total
+    
+    return True
+
+num=int(input("Num: "))
+print(happyNum(num))
