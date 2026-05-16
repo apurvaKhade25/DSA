52b227298d4f4a8658702425aca0426ef7d20f58
apurvaKhade25 <apurvakhade259@gmail.com>
2026-05-16 23:58:00 +0530

    Container with most water

--- Two pointers/ContainerWithMostWater.py
+++ Two pointers/ContainerWithMostWater.py
@@ -1,13 +1,30 @@
-def water(arr):
-    max_A=0
-    for i in range(len(arr)):
-        for j in range(i+1,len(arr)):
-            height=min(arr[i],arr[j])
-            width=j-i
-            area=width*height
-            max_A=max(area,max_A)
+def water(height):
+    # max_A=0
+    # for i in range(len(arr)):
+    #     for j in range(i+1,len(arr)):
+    #         height=min(arr[i],arr[j])
+    #         width=j-i
+    #         area=width*height
+    #         max_A=max(area,max_A)
     
-    return max_A
+    # return max_A
+
+    max_a=0
+    left=0
+    right=len(height)-1
+
+    while left<=right:
+            width=right-left
+            h=min(height[left],height[right])
+            area=width*h
+            max_a=max(area,max_a)
+
+            if height[left]<height[right]:
+                left+=1
+            else:
+                right-=1
+
+    return max_a
 
 # arr=list(map(int,input("Enter array: ").split()))
 arr=[1,8,6,2,5,4,8,3,7]
